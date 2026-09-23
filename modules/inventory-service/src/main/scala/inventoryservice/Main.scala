e8f3d6b9129a22834753e2d0e1ba57a911a76dd7
Paul Beckford <beckfordp@btinternet.com>
2026-09-23 06:20:32 +0100

    feat(inventory-service): Read induced failure/delay config from env vars

--- modules/inventory-service/src/main/scala/inventoryservice/Main.scala
+++ modules/inventory-service/src/main/scala/inventoryservice/Main.scala
@@ -8,10 +8,22 @@ import purerest.docs.Docs
 import purerest.logging.Logging
 import purerest.tracing.{ServerTracing, Tracing}
 
+import scala.concurrent.duration.{Duration, DurationLong}
+
 object Main extends IOApp.Simple {
 
   private val port: Port =
     sys.env.get("INVENTORY_SERVICE_PORT").flatMap(Port.fromString).getOrElse(port"8081")
+
+  private val inducedFailure: InducedFailureConfig =
+    InducedFailureConfig(
+      failureRate = sys.env.get("INVENTORY_INDUCED_FAILURE_RATE").flatMap(_.toDoubleOption).getOrElse(0.0),
+      delay = sys.env
+        .get("INVENTORY_INDUCED_DELAY_MS")
+        .flatMap(_.toLongOption)
+        .map(_.millis)
+        .getOrElse(Duration.Zero)
+    )
 
   val run: IO[Unit] =
     Tracing.console[IO]("inventory-service").use { tracer =>
@@ -21,7 +33,7 @@ object Main extends IOApp.Simple {
         docsRoutes = Docs.routes[IO](
           "Inventory Service",
           "1.0",
-          List(InventoryRoutes.serverEndpoint[IO](store, logger))
+          List(InventoryRoutes.serverEndpoint[IO](store, logger, inducedFailure))
         )
         routes = ServerTracing.middleware(tracer)(docsRoutes)
         _ <- EmberServerBuilder
