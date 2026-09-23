a8c32b984ab4c5f8d18d2ddcebf7968df0c2d376
Paul Beckford <beckfordp@btinternet.com>
2026-09-23 07:38:10 +0100

    test(purerest): Close coverage gap in Metrics.oteljava

--- modules/purerest/src/test/scala/purerest/metrics/MetricsSuite.scala
+++ modules/purerest/src/test/scala/purerest/metrics/MetricsSuite.scala
@@ -2,6 +2,7 @@ package purerest.metrics
 
 import cats.effect.IO
 import munit.CatsEffectSuite
+import purerest.client.HttpClient
 
 class MetricsSuite extends CatsEffectSuite {
 
@@ -12,6 +13,22 @@ class MetricsSuite extends CatsEffectSuite {
         _ <- counter.add(1L)
         metrics <- testMeter.collectMetrics
       } yield assertEquals(metrics.map(_.getName), List("my-counter"))
+    }
+  }
+
+  test("oteljava meter exposes recorded metrics on its Prometheus scrape endpoint") {
+    val scrapePort = 19099
+    Metrics.oteljava[IO]("purerest-oteljava-test", scrapePort).use { meter =>
+      HttpClient.resource[IO].use { client =>
+        for {
+          counter <- meter.counter[Long]("purerest_metrics_suite_probe").create
+          _ <- counter.add(1L)
+          body <- client.expect[String](s"http://localhost:${scrapePort}/metrics")
+        } yield assert(
+          body.contains("purerest_metrics_suite_probe"),
+          s"expected the scrape endpoint to include purerest_metrics_suite_probe, got: $body"
+        )
+      }
     }
   }
 }
